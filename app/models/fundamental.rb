2cd0ea8c0403f020378c2e1825be1bdb1e87767e
Zach Baruch <zbaruch20@gmail.com>
2022-06-07 16:19:09 -0400

    Fundamental validations

--- app/models/fundamental.rb
+++ app/models/fundamental.rb
@@ -1,2 +1,6 @@
 class Fundamental < ApplicationRecord
+  # Validations
+  validates :name, presence: true,
+                   uniqueness: true
+  enum :type, %i(forward backward_lateral horn_slide turn ttr hats_off)
 end
